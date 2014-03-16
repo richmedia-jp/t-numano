a52a936f05c8ae356007d1ae918ecfdabb4655fe
numanomanu <tsuyoshinumano@gmail.com>
2014-03-16 16:26:03 +0900

    routerクラスにresolveメソッドを追加

--- core/Router.php
+++ core/Router.php
@@ -27,4 +27,26 @@ class Router{
 		}
 		return $routes;
 	}
+
+	public function resolve($path_info){
+		if('/' !== substr($path_info,0,1)){
+			$path_info = '/'.$path_info;
+		}
+
+		foreach($this->routes as $pattern => $params){
+			if(preg_match('#^'.$pattern.'$#',$path_info,$matches)){
+				$params = array_merge($params,$matches);
+
+				return $params;
+			}
+		}
+		return false;
+	}
 }
+
+/*
+出力例
+$arrays = array('http://','request.com/','admin');
+$router = new Router($arrays);
+echo $router->resolve('request.com/admin');
+*/
