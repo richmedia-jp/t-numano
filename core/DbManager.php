29a102176ee4da46639f05f76291b9bd237ffab8
numanomanu <tsuyoshinumano@gmail.com>
2014-03-17 14:36:03 +0900

    Responseクラス，DBmanaterクラスの追加

---
+++ core/DbManager.php
@@ -0,0 +1,39 @@
+<?php
+/*
+DBManagerクラス
+*/
+
+class DBManager{
+	protected $connections = array();
+
+	public function connect($name,$params){
+		$params = array_merge(array(
+			'dsn'		=> null,
+			'user'		=> '',
+			'password'	=> '',
+			'options'	=> array(),
+		),$params);
+
+		$con = new PDO(
+			$params['dsn'],
+			$params['user'],
+			$params['password'],
+			$params['options']
+		);
+		$con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+		$this->connections[$name] = $con;
+	}
+
+	public function getConnection($name = null){
+		if(is_null($name)){
+			return current($this->connections);
+		}
+		return $this->connections[$name];
+	}
+}
+/*出力テスト
+$test_array = array();
+$dbmanager = new DBManager();
+echo $dbmanager->connect('name',$test_array);
+*/
